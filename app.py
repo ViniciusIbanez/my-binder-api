8787beb305755f572762d93a2d10b0670b40615c
Vinicius Ibanez <vinicius.ibanez97@gmail.com>
2021-05-08 17:34:45 -0300

    testing to extract and load a MTG set

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from connectors.mongo import connect
 from helpers.secrets_helper import retrieve_secrets
+from etl.extract_cards import *
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,10 +20,21 @@ class HelloWorld(Resource):
             card['id'] = id
             cards.append(card)
         return  jsonify(code=200, data=cards)
+
+class ExtractLoad(Resource):
+    def post(self):
+        request_data = request.get_json()
+        set_code = request_data['set_code']
+        secrets = retrieve_secrets()
+        mongo_connection = connect(credentials=secrets, collection='Cards')
+        set_cards = extract_set(set_code)
+        response = load_set(set_cards, )
+        
+        return jsonify(code = 200 , data=response)
         
 
 
 api.add_resource(HelloWorld, '/cards/all')
-
+api.add_resource(ExtractLoad, '/extract/set')
 if __name__ == '__main__':
     app.run(debug=True)
