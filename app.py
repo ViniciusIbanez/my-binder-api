9645279852b4d376dbb0805f46af3310a125c4fd
ibanez <vinicius.ibanez97@gmail.com>
2021-05-25 04:01:52 -0300

    test

--- app.py
+++ app.py
@@ -11,3 +11,6 @@ api.add_resource(CardsEtl, '/extract/set')
 api.add_resource(User, '/user/insert')
 if __name__ == '__main__':
     app.run(debug=True)
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
